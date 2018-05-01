95025fa49fe864a7250b0c5736ce66b346dfe2bb
Unknown <JPottle776@west-mec.org>
2018-05-01 09:06:17 -0700

    Working on form
    Added to forms still working on the easy form

--- javascript/donation.js
+++ javascript/donation.js
@@ -1,15 +1,38 @@
+/* This is for allowing a custom donation */
+
+//global vars
+var customDonation = document.getElementById("donationAmount");
+var payCustom = document.getElementById("customDonation");
+
+// sets whats originally selected and readonly
 function setReadOnly() {
-    var customDonation = document.getElementById("donationAmount");
-    var payCustom = document.getElementById("customDonation");
+    var select = document.getElementById("select");
+    select.selected = true;
     payCustom.readOnly = true;
-    console.log(customDonation.value);
-    if (payCustom.readOnly == true && customDonation.value == "custom") {
-        payCustom.readOnly = false;
-        console.log("E-YES")
-    }
 }
 
+// lets you add custom donation
+customDonation.onchange = function() {
+    if (customDonation.value == "custom") {
+        payCustom.readOnly = false;
+        document.getElementById("donationLabel").textContent += "*";
+        // document.getElementById("donationLabel").setAttribute("class", "text-danger")
+        console.log("E-YES");
+        console.log(customDonation.value);
+    }
+    // resets the custom donation
+    if (customDonation.value != "custom") {
+        payCustom.readOnly = true;
+        payCustom.value = "";
+        document.getElementById("donationLabel").innerHTML = "Custom Donation";
+    }
+};
 
-
-
+function dontationTotal() {
+    var moneyTotal = document.getElementById("totalDonation");
+    var setDonation = document.getElementById("donationAmount").value;
+    var customAmount = document.getElementById("customDonation").value;
+    moneyTotal.textContent += " $" + setDonation;
+}
+customDonation.addEventListener("change", dontationTotal());
 document.addEventListener("load", setReadOnly());
