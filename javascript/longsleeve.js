a4fcaf157227c9e08a3d17e50b3bdb953b2d28d3
Jacson Bennett <jbennett930@west-mec.org>
2018-05-03 15:32:07 -0700

    edited the long sleeve shirt and made the mobile styles work.

--- javascript/longsleeve.js
+++ javascript/longsleeve.js
@@ -1,3 +1,7 @@
 $('.shirtColor').on('click', function(e) {
     $('#shirt').attr('src', 'images/shirtLong' + ($('.shirtColor').index($(e.target))) + '.jpg');
 });
+
+$('#shirtOptions').change(function() {
+    $("img[name=image-swap]").attr("src", $(this).val());
+});
