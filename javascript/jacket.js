818e9d267e39700cb2ad40dfa4eb6093c4a0b221
Unknown <jbennett930@west-mec.org>
2018-05-04 15:33:08 -0700

    Made the mobile styles work to change the img source.

--- javascript/jacket.js
+++ javascript/jacket.js
@@ -1,3 +1,7 @@
 $('.shirtColor').on('click', function(e) {
     $('#shirt').attr('src', 'images/jacket' + ($('.shirtColor').index($(e.target))) + '.jpg');
 });
+
+$('#shirtOptions').change(function() {
+    $("img[name=image-swap]").attr("src", $(this).val());
+});
--- javascript/shirts.js
+++ javascript/shirts.js
@@ -1,3 +1,7 @@
 $('.shirtColor').on('click', function(e) {
     $('#shirt').attr('src', 'images/shirt' + ($('.shirtColor').index($(e.target)) + 1) + '.jpg');
 });
+
+$('#shirtOptions').change(function() {
+    $("img[name=image-swap]").attr("src", $(this).val());
+});
